ec13ec030d965fbc6abc54bf15f791595d71adb0
ad.bme <you@example.com>
2023-04-03 19:56:59 +0200

    add all glb into all rooms with all animations

---
+++ lib/salles/argentine.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:babylonjs_viewer/babylonjs_viewer.dart';
+import 'package:ttsrooms/test.dart';
+
+import '../TTS-rooms.dart';
+
+
+
+void main() {
+  runApp(pleiades());
+}
+
+class pleiades extends StatelessWidget {
+  const pleiades({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Argentine',
+          style: TextStyle(fontSize: 50),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF212529),
+        leading: Container(
+          child: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.yellow,
+              size: 40,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DropdownButtonApp()),
+              );
+              print("home");
+            },
+          ),
+        ),
+        toolbarHeight: 100, // Set the height here
+      ),
+
+      body: BabylonJSViewer(src: "assets/salles_glb/argentine.glb"),
+      backgroundColor: const Color(0xFF282C31),
+    );
+  }
+}
